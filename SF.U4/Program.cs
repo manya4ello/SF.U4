8c054883923dece28cf7bdcabd857cdb50ea3f0a
Oleg <oleg.freidkin@gmail.com>
2021-12-06 00:48:39 +0300

    всякий треш

--- SF.U4/Program.cs
+++ SF.U4/Program.cs
@@ -4,7 +4,7 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-        string color = "hello";
+         string color = "hello";
 
         while (color != "stop")
         {
@@ -40,7 +40,53 @@ class MainClass
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("цикл дошел до конца");
-
         }
-    }
+            /*
+            Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+        Char [] backname = new char [name.Length];
+        int i = 0;
+        foreach (var ch in name)
+        {    backname[name.Length - 1 - i] = name[i];
+            i++;
+        }
+        Console.Write(backname); 
+
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+        Console.WriteLine(array[0, 0]);
+        foreach (int i in array)
+            Console.WriteLine(i); 
+
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        {
+            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                Console.Write(array[k, i] + " ");
+
+            Console.WriteLine();
+        }
+
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 , 0, -5};
+        int temp;
+        for (var k = 0; k < arr.Length; k++)
+        {
+            for (var i = k + 1; i < arr.Length; i++)
+            {
+                if (arr[k] > arr[i])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[k];
+                    arr[k] = temp;
+                }
+            }
+            Console.Write(arr[k]+" ");
+        }
+        int sum = 0;
+        foreach (var i in arr)
+            sum += i;
+        Console.WriteLine("\n Сумма массива {0}",sum);  */
+
+            Console.ReadKey(); 
+        }
     }
