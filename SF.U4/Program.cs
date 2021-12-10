508c74a4e9aa137bb592e303e379a2dbddd40148
Oleg <oleg.freidkin@gmail.com>
2021-12-10 20:07:39 +0300

    модуль 5

--- SF.U4/Program.cs
+++ SF.U4/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Text;
+
 class MainClass
 {
 	public static void Main(string[] args)
@@ -52,59 +52,147 @@ class MainClass
     }
     Console.Write(backname); 
 
-    int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
     Console.WriteLine(array[0, 0]);
     foreach (int i in array)
         Console.WriteLine(i); 
 
-    int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
     for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
     {
         for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
             Console.Write(array[k, i] + " ");
 
-        Console.WriteLine();
+        Console.WriteLine(); 
     }
 
-        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 , 0, -5};
-    int temp;
-    for (var k = 0; k < arr.Length; k++)
-    {
-        for (var i = k + 1; i < arr.Length; i++)
+        *//*  var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 , 0, -5};
+      int temp;
+      for (var k = 0; k < arr.Length; k++)
+      {
+          for (var i = k + 1; i < arr.Length; i++)
+          {
+              if (arr[k] > arr[i])
+              {
+                  temp = arr[i];
+                  arr[i] = arr[k];
+                  arr[k] = temp;
+              }
+          }
+          Console.Write(arr[k]+" ");
+      }
+      int sum = 0;
+      foreach (var i in arr)
+          sum += i;
+      Console.WriteLine("\n Сумма массива {0}",sum);  
+
+          (string Name, String [] Dishes) User;
+          User.Dishes = new string[5];
+
+          Console.Write("Введите имя: ");
+          User.Name = Console.ReadLine();
+
+
+          for (int i = 0; i < User.Dishes.Length; ++i)
+          {
+              Console.WriteLine("Введите любимое блюдо {0}: ", i + 1);
+              User.Dishes[i] = Console.ReadLine();
+          }
+        
+        int[,] arr = new int[,] { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+        Console.WriteLine(arr.GetUpperBound(0));
+        Console.WriteLine(arr.GetUpperBound(1));
+        int min = 0;
+        for (int i = 0; i <= arr.GetUpperBound(0); i++)
         {
-            if (arr[k] > arr[i])
+            for (int j = 0; j <= arr.GetUpperBound(1); j++)
             {
-                temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
+                for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    if (arr[i, j] > arr[i, k])
+                    {
+                        min = arr[i, k];
+                        arr[i, k] = arr[i, j];
+                        arr[i, j] = min;
+                    }
+                Console.Write(arr[i,j] + " ");                
+            }
+            Console.WriteLine();
+        }  
+        int[][] numbers = new int[3][];
+        numbers[0] = new int[] { 1, 2 };
+        numbers[1] = new int[] { 1, 2, 3 };
+        numbers[2] = new int[] { 1, 2, 3, 4, 5 };
+        foreach (int[] row in numbers)
+        {
+            foreach (int number in row)
+            {
+                Console.Write($"{number} \t");
+            }
+            Console.WriteLine();
+        }
+
+        // перебор с помощью цикла for
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = 0; j < numbers[i].Length; j++)
+            {
+                Console.Write($"{numbers[i][j]} \t");
+            }
+            Console.WriteLine();
+        }  */
+        
+        (string name, int age) anketa;
+
+        Console.Write("Введите имя: ");
+        anketa.name = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами: ");
+        anketa.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя: {0}", anketa.name);
+        Console.WriteLine("Ваш возраст: {0}", anketa.age);
+        ShowColor(anketa.name);
+
+        void ShowColor(string username)
+            {
+            string? color = null;
+            while (color != "stop")
+            {
+                Console.WriteLine("{0}, напишите свой любимый цвет на английском с маленькой буквы", username);
+
+                color = Console.ReadLine();
+
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        continue;
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("цикл дошел до конца");
             }
         }
-        Console.Write(arr[k]+" ");
-    }
-    int sum = 0;
-    foreach (var i in arr)
-        sum += i;
-    Console.WriteLine("\n Сумма массива {0}",sum);  */
 
-
-        StringBuilder sb = new StringBuilder("Привет мир");
-        sb.Append("!");
-        sb.Insert(7, "компьютерный ");
-        Console.WriteLine(sb);
-
-        // заменяем слово
-        sb.Replace("мир", "world");
-        Console.WriteLine(sb);
-
-        // удаляем 13 символов, начиная с 7-го
-        sb.Remove(7, 13);
-        Console.WriteLine(sb);
-
-        // получаем строку из объекта StringBuilder
-        string s = sb.ToString();
-        Console.WriteLine(s);
-
-        Console.ReadKey(); 
+            Console.ReadKey(); 
         }
     }
